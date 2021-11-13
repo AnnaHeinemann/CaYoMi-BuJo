30c421a3d3c334baae6be3b354be16c2964bbdbb
AnnaHeinemann <anna07101986@gmail.com>
2021-11-13 22:56:45 +0100

    Added test for enumeration extensions

---
+++ TestViewModel/TestEnumerationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ViewModel;
+using Xunit;
+
+namespace TestViewModel
+{
+    /// <summary>
+    /// Tests of the enumeration extensions
+    /// </summary>
+    public class TestEnumerationExtensions
+    {
+        /// <summary>
+        /// Tests if the enumeration extensions give the correct result
+        /// </summary>
+        /// <param name="pageTypeDescription"></param>
+        /// <param name="expectedResult"></param>
+        [Theory]
+        [InlineData("Start", PageTypes.Start)]
+        [InlineData("JournalPage", PageTypes.JournalPage)]
+        public void GetValueFromDescription(string pageTypeDescription, PageTypes expectedResult)
+        {
+            Assert.True(expectedResult == EnumerationExtensions.GetValueFromDescription<PageTypes>(pageTypeDescription));
+        }
+    }
+}
