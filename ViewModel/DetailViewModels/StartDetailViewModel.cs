fbc5549691d1ceb52e05450569b7a4b1064cf7b6
AnnaHeinemann <anna07101986@gmail.com>
2021-11-13 22:52:27 +0100

    Added detail view models

---
+++ ViewModel/DetailViewModels/StartDetailViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ViewModel.DetailViewModels
+{
+    public class StartDetailViewModel : BaseDetailViewModel
+    {
+
+        public ICommand SectionSelected { get; private set; }
+
+        public ObservableCollection<SelectorItems> JustANameForTest { get; private set; }
+
+        public StartDetailViewModel()
+        {
+            JustANameForTest = new ObservableCollection<SelectorItems>();
+            JustANameForTest.Add(new SelectorItems() { Name = "Journal", Description = "Journal Page", PageType = PageTypes.JournalPage });
+            JustANameForTest.Add(new SelectorItems() { Name = "Adresses", Description = "All of my adresses", PageType = PageTypes.AdressPage });
+            JustANameForTest.Add(new SelectorItems() { Name = "Habbits", Description = "Habbit trackers", PageType = PageTypes.HabbitTrackerPage });
+
+            SectionSelected = new Command<object>(onSectionSelected, (_) => true);
+        }
+
+        private void onSectionSelected(object args)
+        {
+            if (args == null)
+                throw new ArgumentNullException("Argument of onSectionSelected mustn't be null.");
+
+            if (!(args is SelectorItems selectorItem))
+                throw new ArgumentException("Argument of onSectionSelected has to be of type SelectorItems.");
+
+            PageType = selectorItem.PageType;
+        }
+    }
+
+    public class SelectorItems
+    {
+
+        public PageTypes PageType { get; set; }
+
+        public string Name { get; set; }
+        
+        //public byte[] Icon { get; set; }
+
+        public string Description { get; set; }
+    }
+}
