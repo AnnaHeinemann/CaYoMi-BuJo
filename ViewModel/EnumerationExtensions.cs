d581c6dc2bf978f82a043a2a7d3780bf40b6c933
AnnaHeinemann <anna07101986@gmail.com>
2021-11-13 22:53:55 +0100

    Added extensions for enumerations

---
+++ ViewModel/EnumerationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace ViewModel
+{
+    public static class EnumerationExtensions
+    {
+        /// <summary>
+        /// Get enumeration value from its description
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static T GetValueFromDescription<T>(string description) where T : Enum
+        {
+            foreach (FieldInfo field in typeof(T).GetFields())
+            {
+                DescriptionAttribute customAttribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+                if (customAttribute == null && field.Name == description)
+                {
+                    return (T)field.GetValue(null);
+                }
+                else if (customAttribute != null && customAttribute.Description == description)
+                {
+                    return (T)field.GetValue(null);
+                }
+            }
+
+            throw new ArgumentException("Didn't found.", nameof(description));
+        }
+    }
+}
