3092c0d6af0926926052d810a0fbd6dcc4339d25
AnnaHeinemann <anna07101986@gmail.com>
2021-11-13 22:54:41 +0100

    Added view model factories

--- ViewModel/Factories/BaseViewModelFactory.cs
+++ ViewModel/Factories/BaseViewModelFactory.cs
@@ -1,9 +1,29 @@
-﻿namespace ViewModel.Factories
+﻿using ViewModel.Interfaces;
+
+namespace ViewModel.Factories
 {
     /// <summary>
     /// Base view model factory
     /// </summary>
-    public class BaseViewModelFactory
+    public abstract class BaseViewModelFactory
     {
+        /// <summary>
+        /// Creates a detail view model
+        /// </summary>
+        /// <param name="pageType">Description of the enum of the type of page that is requested</param>
+        /// <returns>Requested view model</returns>
+        public IViewModel CreateViewModel(string pageTypeDescription)
+        {
+            PageTypes pageType = EnumerationExtensions.GetValueFromDescription<PageTypes>(pageTypeDescription);
+
+            return CreateViewModel(pageType);
+        }
+
+        /// <summary>
+        /// Creates a detail view model
+        /// </summary>
+        /// <param name="pageType">Enum of the type of page that is requested</param>
+        /// <returns>Requested view model</returns>
+        public abstract IViewModel CreateViewModel(PageTypes pageType);
     }
 }
--- ViewModel/Factories/DetailViewModelFactory.cs
+++ ViewModel/Factories/DetailViewModelFactory.cs
@@ -1,9 +1,40 @@
-﻿namespace ViewModel.Factories
+﻿using System.Diagnostics;
+using ViewModel.DetailViewModels;
+using ViewModel.Interfaces;
+
+namespace ViewModel.Factories
 {
     /// <summary>
     /// View model factory for detail view models
     /// </summary>
     public class DetailViewModelFactory : BaseViewModelFactory
     {
+        /// <summary>
+        /// Creates a detail view model
+        /// </summary>
+        /// <param name="pageType">Enum of the type of page that is requested</param>
+        /// <returns>Requested view model</returns>
+        public override IViewModel CreateViewModel(PageTypes pageType)
+        {
+            switch (pageType)
+            {
+                case PageTypes.Start:
+                    return new StartDetailViewModel();
+
+                case PageTypes.JournalPage:
+                    return new JournalPageDetailViewModel();
+
+                case PageTypes.AdressPage:
+                    Debug.Assert(false, "Adress");
+                    return null;
+
+                case PageTypes.HabbitTrackerPage:
+                    Debug.Assert(false, "Habbits");
+                    return null;
+
+                default:
+                    return null;
+            }
+        }
     }
 }
