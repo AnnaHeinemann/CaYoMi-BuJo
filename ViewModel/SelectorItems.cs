4b965f0291223aefdcc48a2f63eb3c03eb02e073
AnnaHeinemann <anna07101986@gmail.com>
2021-11-13 22:52:41 +0100

    Added selector items

---
+++ ViewModel/SelectorItems.cs
@@ -0,0 +1,28 @@
+﻿namespace ViewModel
+{
+    /// <summary>
+    /// Item for page type selection
+    /// </summary>
+    public class SelectorItems
+    {
+        /// <summary>
+        /// Type of the page
+        /// </summary>
+        public PageTypes PageType { get; set; }
+
+        /// <summary>
+        /// Name of the item
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Icon of the item
+        /// </summary>
+        //public byte[] Icon { get; set; }
+
+        /// <summary>
+        /// Description of the item
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
