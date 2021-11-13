40ee3787354a6c68fb77676e8ab28b1d1922bc6a
AnnaHeinemann <anna07101986@gmail.com>
2021-11-13 22:57:40 +0100

    Added comments

--- DataAccess/BuJoContext.cs
+++ DataAccess/BuJoContext.cs
@@ -8,20 +8,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
 {
+    /// <summary>
+    /// Database of the bullet journal
+    /// </summary>
     public class BuJoContext : DbContext
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public BuJoContext()
-        {
-            
-            Database.Migrate();
+        {            
+            //Database.Migrate();
         }
 
-        public BuJoContext(DbContextOptions<BuJoContext> options) : base(options)
-        {
-        }
+        /// <summary>
+        /// Constructor using database context options given to base
+        /// </summary>
+        /// <param name="options"></param>
+        public BuJoContext(DbContextOptions<BuJoContext> options) : base(options) {}
 
+        /// <summary>
+        /// Set of journal page entities
+        /// </summary>
         public DbSet<JournalPageEntity> JournalPages { get; set; }
 
+        /// <summary>
+        /// Creates the entity model for journal pages
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<JournalPageEntity>().ToTable("JournalPage");
